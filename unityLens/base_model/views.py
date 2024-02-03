eeeadd5e23ee2598d40424e797e1f864c6ca8c06
realfrankiewilson <frankuwill101@gmail.com>
2024-02-03 05:01:39 +0100

    feat: deleted all files, as a result of error encountered from my environment

--- unityLens/base_model/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def frontpage(request):
-    """A function that returns an html page from template folder"""
-    return render(request, 'core/base.html')
